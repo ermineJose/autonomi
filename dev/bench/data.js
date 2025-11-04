3cf7b3ee512af2600625651c439233c60526d157
github-action-benchmark <github@users.noreply.github.com>
2025-11-04 18:26:45 +0000

    add swarm_driver long handlings (customSmallerIsBetter) benchmark result for 5e42bfe0fe8972f519eb1762c80f613d3042aeae

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1762280803781,
+  "lastUpdate": 1762280805722,
   "repoUrl": "https://github.com/ermineJose/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -106,6 +106,47 @@ window.BENCHMARK_DATA = {
             "name": "Average memory usage w/ upload",
             "value": 14,
             "unit": "MB"
+          }
+        ]
+      }
+    ],
+    "swarm_driver long handlings": [
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "maqi",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "5e42bfe0fe8972f519eb1762c80f613d3042aeae",
+          "message": "Merge pull request #3283 from maqi/transformed_json\n\nfeat(ant): analyze output flatten json as well",
+          "timestamp": "2025-10-31T22:59:13Z",
+          "tree_id": "0db903418adc1fb8cf3f1ea99b46c381c671fce9",
+          "url": "https://github.com/ermineJose/autonomi/commit/5e42bfe0fe8972f519eb1762c80f613d3042aeae"
+        },
+        "date": 1762280804978,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "swarm_driver long handling times",
+            "value": 11464,
+            "unit": "hits"
+          },
+          {
+            "name": "swarm_driver long handling total_time",
+            "value": 20809,
+            "unit": "ms"
+          },
+          {
+            "name": "swarm_driver average long handling time",
+            "value": 1,
+            "unit": "ms"
           }
         ]
       }
